94e0c020608e3970bb7093d6a533ecf5f36159fb
Jack Whetton <j.r.whetton@hotmail.com>
2020-12-03 12:45:37 +0000

    New Changes
    fixed the program the run and display scores. The player again function is was now working.

--- Rock, Paper & Scissors.cs
+++ Rock, Paper & Scissors.cs
@@ -1,130 +1,131 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace RockWins
+namespace RockPaperScissors
 {
-    class program
+    class Program
     {
         static void Main(string[] args)
         {
-            string inputUser, inputComputer; // input Values.
-            int randomInt;
+                string inputPlayer, inputAI; //input values
+                int randomInt;
 
-            bool Playagain = true;
+                bool playAgain = true; 
 
-            while (Playagain)
-            {
-                int userScore = 0; // move here to enable to keep score, when played again.
-                int computerScore = 0;
-
-
-                while (userScore < 3 && computerScore < 3)
+                while (playAgain)
                 {
+                    // scoring variables
+                    int scorePlayer = 0; 
+                    int scoreAI = 0;
 
-
-
-                    Console.WriteLine(" Choose Rock, Paper or Scissors    "); // asking the user tp input there value.
-                    inputUser = Console.ReadLine(); // asking the computer to read the user value.
-                    inputUser = inputUser.ToUpper();// enable upper case letters " i think"
-                    Random rnd = new Random(); // Create a Random Value for the computer to choice.
-
-                    randomInt = rnd.Next(1, 4); // value choice for the computer is between 1 & 4.
-
-                    switch (randomInt) 
+                    while (scorePlayer < 3 && scoreAI < 3) //First to three wins is the winner
                     {
-                        case 1:
-                            inputComputer = "rock";
-                            Console.WriteLine("computer has chosen Rock");
-                            if (inputUser == "rock") // just in case the user and computer chose the same answer.
-                            {
-                                Console.WriteLine("Draw   \n\n");
-                            }
-                            else if (inputUser == "paper") // Other possible outcomes
-                            {
-                                Console.WriteLine("Player Wins \n\n");
-                                userScore++;
-                            }
-                            else if (inputUser == "Scissor")
-                            {
-                                Console.WriteLine("Computer wins \n\n");
-                                computerScore++;
-                            }
-                            break;
 
-                        case 2:
-                            inputComputer = "paper";
-                            Console.WriteLine("computer has chosen Paper");
-                            if (inputUser == "Paper") // just in case the user and computer chose the same answer.
-                            {
-                                Console.WriteLine("Draw \n\n");
-                            }
-                            else if (inputUser == "Rock") // Other possible outcomes
-                            {
-                                Console.WriteLine("Computer Wins!! \n\n");
-                                userScore++;
-                            }
-                            else if (inputUser == "Scissor")
-                            {
-                                Console.WriteLine("Player Wins!! \n\n");
-                                computerScore++;
-                            }
-                            break;
 
-                        case 3:
-                            inputComputer = "scissors";
-                            Console.WriteLine("computer has chosen scissors");
-                            if (inputUser == "scissors") // just in case the user and computer chose the same answer.
-                            {
-                                Console.WriteLine("Draw \n\n");
-                            }
-                            else if (inputUser == "paper") // Other possible outcomes
-                            {
-                                Console.WriteLine("Computer Wins!! \n\n");
-                                userScore++;
-                            }
-                            else if (inputUser == "Rock")
-                            {
-                                Console.WriteLine("Player Wins!! \n\n");
-                                computerScore++;
-                            }
-                            break;
+                        Console.Write("Choose between ROCK, PAPER and SCISSORS:    ");
+                        inputPlayer = Console.ReadLine(); // reads the player choices 
+                        inputPlayer = inputPlayer.ToUpper(); // reads upper case letters
 
-                        default:
-                            Console.WriteLine("Invalid entry"); // if ther input a invalid value
+                        Random rnd = new Random();
 
-                            break;
+                        randomInt = rnd.Next(1, 4); // random varaible that enbale the AI to choose between 1 to 4
+
+                        switch (randomInt)
+                        {
+                            case 1: // The possible outcome for Rock
+                            inputAI = "ROCK";
+                                Console.WriteLine("Computer chose ROCK");
+                                if (inputPlayer == "ROCK")
+                                {
+                                    Console.WriteLine("DRAW!!\n\n");
+                                }
+                                else if (inputPlayer == "PAPER")
+                                {
+                                    Console.WriteLine("PLAYER WINS!!\n\n");
+                                    scorePlayer++;
+                                }
+                                else if (inputPlayer == "SCISSORS")
+                                {
+                                    Console.WriteLine("CPU WINS!!\n\n");
+                                    scoreAI++;
+                                }
+                                break;
+                            case 2: // The possible outcome for Paper
+                                inputAI = "PAPER";
+                                Console.WriteLine("Computer chose PAPER");
+                                if (inputPlayer == "PAPER")
+                                {
+                                    Console.WriteLine("DRAW!!\n\n");
+                                }
+                                else if (inputPlayer == "ROCK")
+                                {
+                                    Console.WriteLine("CPU WINS!!\n\n");
+                                    scoreAI++;
+                                }
+                                else if (inputPlayer == "SCISSORS")
+                                {
+                                    Console.WriteLine("PLAYER WINS!!\n\n");
+                                    scorePlayer++;
+                                }
+                                break;
+                            case 3: // The Possible outcome for Scissors
+                                inputAI = "SCISSORS";
+                                Console.WriteLine("Computer chose SCISSORS");
+                                if (inputPlayer == "SCISSORS")
+                                {
+                                    Console.WriteLine("DRAW!!\n\n");
+                                }
+                                else if (inputPlayer == "ROCK")
+                                {
+                                    Console.WriteLine("PLAYER WINS!!\n\n");
+                                    scorePlayer++;
+                                }
+                                else if (inputPlayer == "PAPER")
+                                {
+                                    Console.WriteLine("CPU WINS!!\n\n");
+                                    scoreAI++;
+                                }
+                                break;
+                            default:
+                                Console.WriteLine("Invalid entry!");
+                                break;
+                        }
+                        // This code will displaying of current scores until there a winners.
+                        Console.WriteLine("\n\nSCORES:\tPLAYER:\t{0}\tCPU:\t{1}", scorePlayer, scoreAI);
+
+                    }
+                    // This program will display text if either player or AI scores reaches three.
+                    if (scorePlayer == 3)
+                    {
+                        Console.WriteLine("Player WON!");
+                    }
+                    else if (scoreAI == 3)
+                    {
+                        Console.WriteLine("AI WON!");
+                    }
+                    else
+                    {
+
+                    }
+                    // This code will execute when game ends, asking player if they want to player again.
+                    Console.WriteLine("Do you want to play again?(y/n)");
+                    string loop = Console.ReadLine();
+                    if (loop == "y")
+                    {
+                        playAgain = true;
+                        Console.Clear();
+                    }
+                    else if (loop == "n")
+                    {
+                        playAgain = false;
+                    }
+                    else
+                    {
+
                     }
 
                 }
-
-                if (userScore == 3) // display text on winner 
-                {
-                    Console.WriteLine(" User Wins !! \n\n");
-                }
-                else if (computerScore == 3)
-                {
-                    Console.WriteLine(" Computer Wins !! \n\n");
-                }
-                else
-                {
-
-                }
-                Console.WriteLine(" Do you want to play again? (Y/N) "); // loop to enable play again option.
-                String loop = Console.ReadLine();
-                if (loop == "y")
-                {
-                    Playagain = true; // change varaiable name 
-                }
-                else if (loop == "n")
-                {
-                    Playagain = false;
-                }
-
-
             }
         }
+    
     }
-}
+
